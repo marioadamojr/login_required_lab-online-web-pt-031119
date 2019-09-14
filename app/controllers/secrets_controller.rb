a1b104dca5bb80323cd42c4dfadfb45b39386637
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 14:44:47 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,14 @@
 class SecretsController < ApplicationController
-  def new
+  before_action :require_login
+
+  def show
+    if current_user!
+      redirect_to '/login'
+  end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 end
