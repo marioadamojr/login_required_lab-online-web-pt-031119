994db413cdee6c19c09d3ce1763a8d8441c23b41
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 14:49:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
-  before_action :require_login
 
   def new
   end
 
-  def show
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
 
+  def destroy
+    session.delete :name
+    redirect_to '/'
   end
 end
