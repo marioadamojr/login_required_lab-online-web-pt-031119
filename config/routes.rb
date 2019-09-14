417207df91e01ea8dc7a5613741c5687a66e6420
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-14 15:05:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post '/create', to: 'sessions#create'
   post '/destroy', to: 'sessions#destroy'
 
+  root '/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
